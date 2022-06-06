45275c00ec56606c3ba3066ef197bfab7627ffeb
fuaus <kotingfunk@gmail.com>
2022-06-06 18:39:10 -0400

    pseudo code

---
+++ schedule.js
@@ -0,0 +1,8 @@
+// step 1: put current date using moment()
+// step 2: comapre the current hour with time-blocks
+// step 3: use the cs classes for present, past, or future compare to the time-blocks with moment() 
+    // if moment() > time-blocks => past
+    // if moment() == time-blocks => present
+    // if moment() < time-blocks => future
+
+// step 4: grab textarea value and stoer it in localstorage
